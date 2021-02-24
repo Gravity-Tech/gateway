b3e6d80c85a24f4cff2403a30a1d280034257df2
Shamil Khalilov <shamfama@gmail.com>
2021-02-24 23:33:01 +0300

    Add deps

--- tests/gateway.test.ts
+++ tests/gateway.test.ts
@@ -3,7 +3,5 @@
 
 
 describe('gateway tests', () => {
-	
-
-
+  
 })
--- tests/utils.ts
+++ tests/utils.ts
@@ -1,27 +1,41 @@
-const { seedUtils } = require('@waves/waves-transactions');
+const { seedUtils } = require("@waves/waves-transactions");
 const { Seed } = seedUtils;
 
-function generateSeeds(chain_id) {
-	const keys = 'Distributor,Gravity,Nebula,Port'.split(',')
-		.reduce((acc, key) => {
-			const chainId = chain_id.charCodeAt(0)
-			const instance = new Seed(Seed.create().phrase, chainId);
-			let { address, phrase: seed, keyPair } = instance;
-			const { publicKey, privateKey } = keyPair
+type DeploymentSeeds<V> = {
+  Distributor: V;
+  Gravity: V;
+  Nebula: V;
+  Port: V;
+};
 
-			return {
-				...acc,
-				[key]: { address, seed, publicKey, privateKey } 
-			}
-		}, {})
-	
-	return {
-		...keys,
-		Seeds: {
-			"DistributionSeed": keys.Distributor.seed,
-			"GravityContractSeed": keys.Gravity.seed,
-  			"NebulaContractSeed": keys.Nebula.seed,
-  			"SubscriberContractSeed": keys.Port.seed,
-		}
-	}
+type AccountData = {
+  address: string;
+  seed: string;
+  publicKey: string;
+  privateKey: string;
+}
+
+function generateSeeds(chain_id): DeploymentSeeds<AccountData> {
+  const keys = "Distributor,Gravity,Nebula,Port".split(",").reduce((acc, key) => {
+    const chainId = chain_id.charCodeAt(0);
+    const instance = new Seed(Seed.create().phrase, chainId);
+    let { address, phrase: seed, keyPair } = instance;
+    const { publicKey, privateKey } = keyPair;
+
+    return {
+      ...acc,
+      [key]: { address, seed, publicKey, privateKey },
+    };
+  }, {} as DeploymentSeeds<AccountData>);
+
+  return keys
+  // return {
+  //   ...keys,
+  //   Seeds: {
+  //     DistributionSeed: keys.Distributor.seed,
+  //     GravityContractSeed: keys.Gravity.seed,
+  //     NebulaContractSeed: keys.Nebula.seed,
+  //     SubscriberContractSeed: keys.Port.seed,
+  //   },
+  // };
 }
